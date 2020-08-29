e5e0b190411e0c90ee02db0e4bc3d8415c0ca79f
hopogo <bopekri@gmail.com>
2020-08-29 12:28:54 +0200

    model and viewmodels

--- JwtAuthentication/JwtAuthentication/ViewModels/MyIdentityUser.cs
+++ JwtAuthentication/JwtAuthentication/ViewModels/MyIdentityUser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -6,20 +7,13 @@ using System.Threading.Tasks;
 
 namespace JwtAuthentication.ViewModels
 {
-    public class RegisterViewModel
+    public class MyIdentityUser : IdentityUser
     {
-        [Required]
-        [EmailAddress]
-        public string Email { get; set; }
-
-        [Required]
-        public string Password { get; set; }
-
         [Required]
         public double UserWeight { get; set; }
 
         [Required]
         public string DeviceID { get; set; }
-    }
 
+    }
 }
