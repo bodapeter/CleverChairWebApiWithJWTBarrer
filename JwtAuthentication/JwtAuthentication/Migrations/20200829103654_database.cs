7e8bdc52410debed38687d87ee5aa5b0554591d8
hopogo <bopekri@gmail.com>
2020-08-29 12:41:01 +0200

    migrácio és update

---
+++ JwtAuthentication/JwtAuthentication/Migrations/20200829103654_database.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JwtAuthentication.Migrations
+{
+    public partial class database : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SensorDatas",
+                columns: table => new
+                {
+                    SensorId = table.Column<string>(nullable: false),
+                    DeviceId = table.Column<string>(nullable: true),
+                    TimeStamp = table.Column<DateTime>(nullable: false),
+                    TopOfLeftSensor = table.Column<float>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SensorDatas", x => x.SensorId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(nullable: false),
+                    DeviceID = table.Column<string>(nullable: true),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    High = table.Column<double>(nullable: false),
+                    Weigh = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Devices",
+                columns: table => new
+                {
+                    DeviceId = table.Column<string>(nullable: false),
+                    UserId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Devices", x => x.DeviceId);
+                    table.ForeignKey(
+                        name: "FK_Devices_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Devices_UserId",
+                table: "Devices",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SensorDatas_DeviceId",
+                table: "SensorDatas",
+                column: "DeviceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_DeviceID",
+                table: "User",
+                column: "DeviceID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SensorDatas_Devices_DeviceId",
+                table: "SensorDatas",
+                column: "DeviceId",
+                principalTable: "Devices",
+                principalColumn: "DeviceId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_User_Devices_DeviceID",
+                table: "User",
+                column: "DeviceID",
+                principalTable: "Devices",
+                principalColumn: "DeviceId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Devices_User_UserId",
+                table: "Devices");
+
+            migrationBuilder.DropTable(
+                name: "SensorDatas");
+
+            migrationBuilder.DropTable(
+                name: "User");
+
+            migrationBuilder.DropTable(
+                name: "Devices");
+        }
+    }
+}
